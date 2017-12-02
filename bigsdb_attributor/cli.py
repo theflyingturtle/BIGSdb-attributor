2451d513142dc364c957ab02ad2e9a26ee36de50
Melissa Jansen van Rensburg <mj.vanr@gmail.com>
2017-12-02 23:06:25 +0000

    formatting

--- bigsdb_attributor/cli.py
+++ bigsdb_attributor/cli.py
@@ -1,5 +1,8 @@
 import argparse
 import logging
+import shutil
+import sys
+import os
 
 
 def parse_args():
@@ -10,16 +13,16 @@ def parse_args():
     parser.add_argument("--overwrite", help="Overwrite output directory if it already exists", action="store_true")
     parser.add_argument("--logfile", help="name of log file (default: %(default)s)", default="Prep.log")
     parser.add_argument("--sourcelookup", required=True, help="CSV file containing two columns mapping allowed source values from PubMLST to aggregated attribution source.  Headers must be 'original' and 'aggregated' in that order.")
-    parser.add_argument("--mode", help="Attribution program to use", required=True, choices=["structure",'isource'])
+    parser.add_argument("--mode", help="Attribution program to use", required=True, choices=["structure", 'isource'])
     parser.add_argument("--attributor-path", help="Full path to STRUCTURE or iSource", required=True)
 
     return parser.parse_args()
 
 
-def setup_output_directory(output):
+def setup_output_directory(output, overwrite):
     if os.path.isdir(output):
-        if args.overwrite:
-            logging.info("Directory called '%s' already exists. Clearing it.", output) 
+        if overwrite:
+            logging.info("Directory called '%s' already exists. Clearing it.", output)
             shutil.rmtree(output)
         else:
             logging.fatal("Directory called '%s' already exists, choose a new name or use the overwrite option.", output)
@@ -28,12 +31,15 @@ def setup_output_directory(output):
     logging.info("Creating new directory called '%s'", output)
     os.mkdir(output)
 
+
 def main():
     logging.basicConfig(level=logging.DEBUG)
     print('hello')
     args = parse_args()
-    setup_output_directory(args.output_directory)
+    setup_output_directory(args.output_directory, args.overwrite)
+    fh = logging.FileHandler(os.path.join(args.output_directory, args.logfile))
+    logging.getLogger().addHandler(fh)
 
 
 if __name__ == '__main__':
-	main()
+    main()
