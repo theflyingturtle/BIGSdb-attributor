3a5208b252cfaf82e45d7129cd7dba6e6047bcdc
Melissa Jansen van Rensburg <mj.vanr@gmail.com>
2017-12-02 21:30:24 +0000

    Add commandline interface

--- bigsdb_attributor/bigsdb_attributor.py
+++ bigsdb_attributor/bigsdb_attributor.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""Main module."""
+"""Written by Melissa Jansen van Rensburg and Katriel Cohn-Gordon (2017).
+
+Takes test and reference sets from BIGSdb, converts to STRUCTURE/iSource format, runs STRUCTURE/iSource, and parses outputs.
+"""
--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     author_email='m.jansenvr@imperial.ac.uk',
     url='https://github.com/theflyingturtle/bigsdb_attributor',
     packages=find_packages(include=['bigsdb_attributor']),
+    entry_points={'console_scripts':['bigsdb_attributor=bigsdb_attributor.cli:main']},
     include_package_data=True,
     install_requires=requirements,
     license="GNU General Public License v3",
